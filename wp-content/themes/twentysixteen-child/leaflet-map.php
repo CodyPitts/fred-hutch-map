02047f0ccbc1ab6f9326334573639cbd15b89942
MrTaylorFan123 <aceusilly@verizon.net>
2016-02-16 11:53:56 -0800

    Updated Leaflet Map file

--- wp-content/themes/twentysixteen-child/leaflet-map.php
+++ wp-content/themes/twentysixteen-child/leaflet-map.php
@@ -17,11 +17,22 @@
 <body>
 <div id="map" style="width: 960px; height: 315px"></div>
 
+
 <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
 <script>
 
-//setup map
+//get URLs for images
+// this part holds "http://host_name"
+var urlBegin = window.location.protocol + "//" + window.location.host;
 
+// this part gets everything after host_name
+var pathArray = window.location.pathname.split( '/' );
+var directoryFolder = pathArray[1];
+
+//put them together with folder path ( http://localhost:888/wordpresstest/wp-content/uploads/2016 )
+urlBegin = urlBegin + "/" + directoryFolder + "/wp-content/uploads/2016";
+
+// setup map
 var map = L.map('map', {
                 zoomControl: false,
                 dragging: false,
@@ -33,7 +44,7 @@ var map = L.map('map', {
                 });
 var w = 1920,
     h = 631,
-    url = 'http://localhost:8888/wordpresstest/wp-content/uploads/2016/01/FH_Angle_2.png';
+    url = urlBegin + '/01/FH_Angle_2.png';
 
 // calculate the edges of the image, in coordinate space
 var southWest = map.unproject([0, h], map.getMaxZoom()-1);
@@ -97,14 +108,14 @@ function resetStyle(e) {
 }
 
 //switch angle when a hotzone is clicked
-function changeAngle(e, map, initialView){
+function changeAngle(e, map, initialView, hotspot2){
 
     //switch camera angle
-    var initialView2 = new L.ImageOverlay('http://localhost:8888/wordpresstest/wp-content/uploads/2016/01/FH_Angle_7.png', [[ center_h * 2, 0 ], [ 0, center_w * 2]]);
+    var initialView2 = new L.ImageOverlay(urlBegin + '/01/FH_Angle_7.png', [[ center_h * 2, 0 ], [ 0, center_w * 2]]);
     map.addLayer(initialView2);
     
     //add a test hotspot to the new image
-    var hotspot1 = L.marker([110, 310]);
+    var hotspot1 = L.marker([105, 312]);
     hotspot1.bindPopup("<b>Employee Parking</b>");
     var parking = L.layerGroup([hotspot1]);
    
@@ -117,7 +128,7 @@ function changeAngle(e, map, initialView){
     
     //define the back button
     var goBackIcon = L.icon({
-        iconUrl: 'http://localhost:8888/wordpresstest/wp-content/uploads/2016/02/back_arrow-e1454308500467.png',
+        iconUrl: urlBegin + '/02/edited_back_arrow.png',
         iconSize: [40,40]
     });
     
@@ -134,12 +145,17 @@ function changeAngle(e, map, initialView){
                   
         //add back initial map angle layer
         map.addLayer(initialView);
+        map.addLayer(hotspot2);
     });
 }
 
 //create quad1 and add to map
 quad1 = createHotzones();
 map.addLayer(quad1);
+
+var hotspot2 = L.marker([105, 175]);
+hotspot2.bindPopup("<b>Thomas</b>");
+hotspot2.addTo(map);
 
 // mouseover and mouseout for hotzone
 quad1.on('mouseover', function(e){
@@ -151,7 +167,8 @@ quad1.on('mouseout', function(e){
 
 //hotzone click
 quad1.on('click', function(e){
-         changeAngle(e, map, initialView);
+         map.removeLayer(hotspot2);
+         changeAngle(e, map, initialView, hotspot2);
 });
 
 
