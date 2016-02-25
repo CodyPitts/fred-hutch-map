15fe1e94d9aa071228576fdb2f5a5318a548bfa5
MrTaylorFan123 <aceusilly@verizon.net>
2016-02-25 14:05:14 -0800

    Path POC with Jake's PNGs

--- wp-content/themes/twentysixteen-child/leaflet-map.php
+++ wp-content/themes/twentysixteen-child/leaflet-map.php
@@ -15,17 +15,19 @@
     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
 </head>
 <body>
-<div id="map" style="width: 960px; height: 315px"></div>
+<div id="map" style="width: 1200px; height: 675px"></div>
 
 
 <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
 <script>
 
+//FOR ABOVE MAP STYLE: width: 960px; height: 315px
+
 //get URLs for images
 // this part holds "http://host_name"
 var urlBegin = window.location.protocol + "//" + window.location.host;
 
-// this part gets everything after host_name
+// this part gets everything after host_name up until the individual folders holding the images
 var pathArray = window.location.pathname.split( '/' );
 var directoryFolder = pathArray[1];
 
@@ -42,19 +44,19 @@ var map = L.map('map', {
                 boxZoom: false,
                 crs: L.CRS.Simple
                 });
-var w = 1920,
-    h = 631,
-    url = urlBegin + '/01/FH_Angle_2.png';
+var w = 1200,
+    h = 675,
+    url = urlBegin + '/02/FredHutch_Home-1200x675.png';
 
 // calculate the edges of the image, in coordinate space
 var southWest = map.unproject([0, h], map.getMaxZoom()-1);
 var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
 var bounds = new L.LatLngBounds(southWest, northEast);
 
-// setup map dimensions
+// setup map dimensions, change x in zoom * x if dimensions are off
 var zoom = 1;
-var center_h = h / (zoom * 8),
-    center_w = w / (zoom * 8);
+var center_h = h / (zoom * 4),
+    center_w = w / (zoom * 4);
 
 map.setView([center_h, center_w], zoom);
 
@@ -62,8 +64,8 @@ var initialView = new L.imageOverlay( url, [[ center_h * 2, 0 ], [ 0, center_w *
 map.addLayer(initialView);
 
 function createHotzones(){
-    //add a hotzone
-    var quad1 = L.polygon([[160,1],[160,175],[105,205],[70,1]], {
+    //add a hotzone for eastlake
+    var eastlakeZone = L.polygon([[210,30],[215,195],[190,200],[170,30]], {
                           weight: 2,
                           opacity: 1,
                           color: 'green',
@@ -71,7 +73,7 @@ function createHotzones(){
                           dashArray: '4'
                           });
     // return the hotzone to add to the map outside the function
-    return quad1;
+    return eastlakeZone;
 }
 
 //highlight for hotzones on mouseover
@@ -107,68 +109,100 @@ function resetStyle(e) {
     }
 }
 
-//switch angle when a hotzone is clicked
-function changeAngle(e, map, initialView, hotspot2){
+//switch to Eastlake angle when eastlakeZone is clicked
+function changeEastlakeAngle(e, map, initialView, hotspot2){
 
-    //switch camera angle
-    var initialView2 = new L.ImageOverlay(urlBegin + '/01/FH_Angle_7.png', [[ center_h * 2, 0 ], [ 0, center_w * 2]]);
-    map.addLayer(initialView2);
+    // Preload images
+    var sequence = new Array()
+    for(i = 1; i < 66; i++) {
+        if (i < 10)
+            sequence[i] = new L.ImageOverlay(urlBegin + '/02/FredHutch_EastLakePath0' + i + '-1200x675.png', [[ center_h * 2, 0 ], [ 0, center_w * 2]]);
+
+        else
+            sequence[i] = new L.ImageOverlay(urlBegin + '/02/FredHutch_EastLakePath' + i + '-1200x675.png', [[ center_h * 2, 0 ], [ 0, center_w * 2]]);
+    }
+    
     
     //add a test hotspot to the new image
-    var hotspot1 = L.marker([105, 312]);
-    hotspot1.bindPopup("<b>Employee Parking</b>");
-    var parking = L.layerGroup([hotspot1]);
-   
+    var hotspot1 = L.marker([155, 360]);
+    hotspot1.bindPopup("<b>Skybridge</b>");
+    var miscellaneous = L.layerGroup([hotspot1]);
+    
     //menu basics
     var filters = {
-        "Parking": parking
+        "Miscellaneous": miscellaneous
     };
-    var mapMenu = L.control.layers(null,filters).addTo(map);
-    
+    var mapMenu = L.control.layers(null,filters);
     
     //define the back button
     var goBackIcon = L.icon({
-        iconUrl: urlBegin + '/02/edited_back_arrow.png',
-        iconSize: [40,40]
-    });
+                            iconUrl: urlBegin + '/02/edited_back_arrow.png',
+                            iconSize: [80,80]
+                            });
     
-    //add back button to the map
-    var backButton = L.marker([145, 10], {icon: goBackIcon}).addTo(map);
+    //define back button marker for the map
+    var backButton = L.marker([320, 15], {icon: goBackIcon});
     
+    
+    // add all layers for path to zoom in
+    map.addLayer(sequence[1]);
+    var i = 2;
+    setInterval(function(){
+                map.addLayer(sequence[i]);
+                map.removeLayer(sequence[i-1]);
+                i++;
+                
+                //when all layers have been added, add back the back button and menu
+                if (i == 65){
+                    mapMenu.addTo(map);
+                    backButton.addTo(map);
+                }
+                
+                
+    }, 50, 65);
+    
+     
     //event to go back when the back button is clicked
     backButton.on('click', function(e){
-        //remove new layers
-        map.removeLayer(initialView2);
         map.removeLayer(hotspot1);
         map.removeLayer(backButton);
         mapMenu.removeFrom(map);
                   
-        //add back initial map angle layer
-        map.addLayer(initialView);
-        map.addLayer(hotspot2);
+        //remove all layers and get back to the home image
+        var i = 65;
+        setInterval(function(){
+                    map.removeLayer(sequence[i]);
+                    map.addLayer(sequence[i-1]);
+                    i--;
+                    
+                    // when the initial layer is finally added back on, put the name hotspot back
+                    if(i == 1){
+                        map.addLayer(hotspot2);
+                    }
+        }, 50, 64);
     });
 }
 
-//create quad1 and add to map
-quad1 = createHotzones();
-map.addLayer(quad1);
+//create eastlakeZone and add to map
+eastlakeZone = createHotzones();
+map.addLayer(eastlakeZone);
 
-var hotspot2 = L.marker([105, 175]);
-hotspot2.bindPopup("<b>Thomas</b>");
+var hotspot2 = L.marker([165, 240]);
+hotspot2.bindPopup("<b>Thomas Building</b>");
 hotspot2.addTo(map);
 
 // mouseover and mouseout for hotzone
-quad1.on('mouseover', function(e){
+eastlakeZone.on('mouseover', function(e){
          highlightFeature(e);
 });
-quad1.on('mouseout', function(e){
+eastlakeZone.on('mouseout', function(e){
          resetStyle(e);
 });
 
-//hotzone click
-quad1.on('click', function(e){
+//Eastlake Hotzone click
+eastlakeZone.on('click', function(e){
          map.removeLayer(hotspot2);
-         changeAngle(e, map, initialView, hotspot2);
+         changeEastlakeAngle(e, map, initialView, hotspot2);
 });
 
 
